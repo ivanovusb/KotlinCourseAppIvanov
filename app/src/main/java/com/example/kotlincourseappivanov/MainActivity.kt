d8b994686234a3ce5c547bc1b6a0cb884f213ebe
aleksivanov <ivanov.aleksey.job@gmail.com>
2022-06-28 20:20:12 +1000

    Задание 4 | Повесил кнопку и добавил к ней ClickListener в MainActivity

--- app/src/main/java/com/example/kotlincourseappivanov/MainActivity.kt
+++ app/src/main/java/com/example/kotlincourseappivanov/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.kotlincourseappivanov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.example.kotlincourseappivanov.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnPressMe : Button = findViewById(R.id.press_me_button)
+
+
+        btnPressMe.setOnClickListener {
+            Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT).show()
+
+
+
+        }
     }
 }
