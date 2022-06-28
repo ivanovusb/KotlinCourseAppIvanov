ee8d6d77c22a956ed2db268b5e493ce8e10915d1
aleksivanov <ivanov.aleksey.job@gmail.com>
2022-06-28 20:16:49 +1000

    Задание 2 и 3 | Перевел проект и MainActivity в Kotlin

--- app/src/main/java/com/example/kotlincourseappivanov/MainActivity.kt
+++ app/src/main/java/com/example/kotlincourseappivanov/MainActivity.kt
@@ -1,14 +1,12 @@
-package com.example.kotlincourseappivanov;
+package com.example.kotlincourseappivanov
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlincourseappivanov.R
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
