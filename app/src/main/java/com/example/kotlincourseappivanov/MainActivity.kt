80bb2a2df5a2afac2a16fbdafeab0ff49840ba87
aleksivanov <ivanov.aleksey.job@gmail.com>
2022-06-28 20:32:33 +1000

    Задание 5 | Выполнил все пункты из задания - создал DataClass, в нем выполнил все подпункты

--- app/src/main/java/com/example/kotlincourseappivanov/MainActivity.kt
+++ app/src/main/java/com/example/kotlincourseappivanov/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlincourseappivanov
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.kotlincourseappivanov.R
 
@@ -11,14 +12,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnPressMe : Button = findViewById(R.id.press_me_button)
-
+        val btnPressMe: Button = findViewById(R.id.press_me_button)
+        val someData = SomeDataClass(123, "id")
+        val txtViewOne: TextView = findViewById(R.id.text)
+        val txtViewTwo: TextView = findViewById(R.id.text_2)
+        val someDataTwo = someData.copy()
 
         btnPressMe.setOnClickListener {
             Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT).show()
+            txtViewOne.text = someData.toString()
+            txtViewTwo.text = someDataTwo.toString()
 
+        }
 
+        someCycles()
+    }
 
+    fun someCycles() {
+        val array = intArrayOf(1, 5, 6, 23, 12, 4, 12, 65, 43, 21, 1, 9, 15)
+        for (i in array.indices) {
+            println(array[i])
+        }
+
+        for (i in 20 downTo 1 step 3) {
+            println(i)
+        }
+
+        for (item in array) {
+            println(item)
+        }
+
+        var index = 0
+        while (index < array.size) {
+            println("Item at $index is ${array[index]}")
+            index++
         }
     }
 }
