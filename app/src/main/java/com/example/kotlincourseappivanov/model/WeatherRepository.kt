aa7507a5f176bb94e3182899be9f1fd0d9b15f98
aleksivanov <ivanov.aleksey.job@gmail.com>
2022-07-15 16:44:03 +1000

    Начало задания 3 | Переделал репозиторий на отдельные функциональные интерфейсы, добавил Location

--- app/src/main/java/com/example/kotlincourseappivanov/model/WeatherRepository.kt
+++ app/src/main/java/com/example/kotlincourseappivanov/model/WeatherRepository.kt
@@ -2,7 +2,15 @@ package com.example.kotlincourseappivanov.model
 
 import com.example.kotlincourseappivanov.Weather
 
-interface WeatherRepository {
-    fun getWeather(lat: Double, lon: Double): Weather
+fun interface RepositorySingle {
+    fun getWeather()
+}
+
+fun interface RepositoryMulti {
     fun getListWeather(): List<Weather>
+}
+
+sealed class Location{
+    object Russia: Location()
+    object World: Location()
 }
